b611c2765298c4e16fd84d6b360ff82ce4f9073f
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-01-28 16:51:08 +0200

    create function onStart

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import './sass/main.scss';
 import getRefs from './js/get-refs';
+import onLoadPage from './js/onStart';
 import MoviesApi from './js/api-requests';
 import moviesMarkUp from './js/movies-grid';
-import movieDetailMarkUp from './js/modal-movie-details';
 
 const movies = new MoviesApi();
 const refs = getRefs();
 
 refs.form.addEventListener('submit', onFormSubmit);
-
-function onStart() {
-  refs.gallery.innerHTML = '';
-
-  movies.resetPage();
-  movies.getPopularMovies().then(response => {
-    moviesMarkUp(response.data.results);
-    getIdFromCards();
-  });
-}
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -33,19 +23,4 @@ function onFormSubmit(e) {
   });
 }
 
-function onSearchMovieById() {
-  movies.getMoviesById().then(response => {
-    console.log(response.data);
-    movieDetailMarkUp(response.data); // Модалка полной информации о фильме
-  });
-}
-
-onStart();
-
-function getIdFromCards() {
-  const moviePoster = document.querySelector('.gallery');
-  moviePoster.addEventListener('click', e => {
-    movies.id = e.target.dataset.id;
-    onSearchMovieById();
-  });
-}
+onLoadPage();
