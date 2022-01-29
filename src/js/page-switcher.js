1c7050b484bfdda38bbec05ad07eac37ffb20f17
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-01-29 15:45:17 +0200

    Update page-switcher.js

--- src/js/page-switcher.js
+++ src/js/page-switcher.js
@@ -1,11 +1,10 @@
 const refs = {
-    libraryBtnEl: document.querySelector('.library-link'),
-    galleryEl: document.querySelector('.gallery'),
+  libraryBtnEl: document.querySelector('.library-link'),
+  galleryEl: document.querySelector('.gallery'),
 };
 
-
 function onLibraryBtnActive(string) {
-    refs.galleryEl.innerHTML = string;
+  refs.galleryEl.innerHTML = string;
 }
 
 refs.libraryBtnEl.addEventListener('click', () => {
