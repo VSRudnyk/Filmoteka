862c99b7c4e95e5ee72566f5cc8b1ff189ec22d3
Ihor Khomenko <igorhomenko97@gmail.com>
2022-01-29 21:58:39 +0200

    Update get-refs.js

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,7 +2,5 @@ export default function getRefs() {
   return {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
-    btnAddWatched: document.querySelector('.watched'),
-    btnAddQueue: document.querySelector('.queue'),
   };
 }
