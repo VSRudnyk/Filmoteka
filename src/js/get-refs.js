c6a020e541c13b489d8a660196265edb5129cff2
Ihor Khomenko <igorhomenko97@gmail.com>
2022-01-29 21:43:14 +0200

    add localStorage

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,5 +2,7 @@ export default function getRefs() {
   return {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
+    btnAddWatched: document.querySelector('.watched'),
+    btnAddQueue: document.querySelector('.queue'),
   };
 }
