ca674a56909651234ce22a6a9b2fa4f121638894
Maxim Samsonenko <samsonenko.max@gmail.com>
2022-01-30 14:41:17 +0200

    fixed absent poster/genres and year info

--- src/js/movies-grid.js
+++ src/js/movies-grid.js
@@ -7,17 +7,16 @@ export default function moviesMarkUp(data) {
   const markUp = data
     .map(({ poster_path, title, genre_ids, release_date, id }) => {
       const genresToCards = createGenresText(genre_ids);
-      const year = release_date.slice(0, 4);
       return `
       <div class="movie-card">
         <a class="movie-link">
-        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" class="movie-poster" data-id="${id}"/>
+        <img src="${setPoster(poster_path)}" alt="" class="movie-poster" data-id="${id}"/>
         <div class="movie-info">
             <p class="movie-title">${title.toUpperCase()}</p>
             <div class="movie-genres-year-wrapper">
               <span class="movie-genres">${genresToCards}</span>
               <div class="movie-year-wrapper">
-                <span class="movie-years">${year}</span>
+                <span class="movie-years">${setReleaseDate(release_date)}</span>
               </div>
             </div>
         </div>
@@ -27,4 +26,19 @@ export default function moviesMarkUp(data) {
     .join('');
 
   return refs.gallery.insertAdjacentHTML('beforeend', markUp);
+}
+
+function setPoster(poster) {
+  if (poster === null) {
+    return 'https://wipfilms.net/wp-content/data/posters/tt0338683.jpg';
+  }
+
+  return `https://image.tmdb.org/t/p/w500${poster}`;
+}
+
+function setReleaseDate(year) {
+  if (year.length === 0) {
+    return 'No release date available';
+  }
+  return year.slice(0, 4);
 }
