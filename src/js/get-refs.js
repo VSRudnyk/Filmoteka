2904664337a9e050daa037d6b2f12adef8eec8c0
Mashtaler Olha <mashtaler887@gmail.com>
2022-01-31 22:12:24 +0200

    add input listener

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -9,5 +9,8 @@ export default function getRefs() {
     homePage: document.querySelector('.home-link'),
     headerByClass: document.querySelector('.header'),
     headerByTeg: document.querySelector('header'),
+    search: document.querySelector('.search-list'),
+    searchItem: document.querySelector('.search-item'),
+    searchInput: document.querySelector('.search_input'),
   };
 }
---
+++ src/js/range-of-search-film.js
@@ -0,0 +1,80 @@
+import getRefs from '../js/get-refs';
+import debounce from 'debounce';
+import MoviesApi from '../js/api-requests';
+import moviesMarkUp from '../js/movies-grid';
+import onLoadPage from '../js/onStart';
+
+const movies = new MoviesApi();
+const refs = getRefs();
+
+refs.form.addEventListener('input', debounce(onInputRenderCart, 500));
+refs.form.addEventListener('input', debounce(onInput, 500));
+refs.form.addEventListener('input', removeHiddenClass);
+
+function onInput(e) {
+  e.preventDefault();
+
+  const searchInput = refs.searchInput.value;
+  if (searchInput === '') {
+    refs.search.innerHTML = '';
+    return;
+  }
+
+  displayResults(searchInput, renderFilmList);
+}
+
+function onInputRenderCart(e) {
+  e.preventDefault();
+
+  const searchInput = refs.searchInput.value;
+  if (searchInput === '') {
+    onLoadPage();
+    return;
+  }
+
+  refs.gallery.innerHTML = '';
+
+  displayResults(searchInput, moviesMarkUp);
+}
+
+function displayResults(searchInput, callback) {
+  movies.query = searchInput;
+  movies.resetPage();
+
+  movies.getSearchMovies().then(response => {
+    callback(response.data.results);
+  });
+}
+
+function renderFilmList(films) {
+  if (films.length > 0) {
+    handleRangeOfResults(films.slice(0, 5));
+    return;
+  }
+}
+
+function handleRangeOfResults(films) {
+  refs.search.innerHTML = '';
+
+  const fill = films
+    .map(film => {
+      return `<li class="search-item" data-id=${film.id}>${film.title}</li>`;
+    })
+    .join('');
+
+  return refs.search.insertAdjacentHTML('beforeend', fill);
+}
+
+function removeHiddenClass(e) {
+  e.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = e.currentTarget;
+
+  refs.search.classList.remove('is-hidden');
+
+  if (searchQuery.value === '') {
+    refs.search.classList.add('is-hidden');
+    return;
+  }
+}
