94f14b8d1715b488563ddf471231c0e73a11386d
Anastasiia Tyshkevych <anastasia.tyshkevych@gmail.com>
2022-02-06 19:42:00 +0200

    swicher-theme

--- src/index.js
+++ src/index.js
@@ -27,3 +27,32 @@ function onFormSubmit(e) {
 }
 
 onLoadPage();
+
+
+
+
+document.querySelector('.themetoggle').addEventListener('click', (event)=>{
+  event.preventDefault();
+  if (localStorage.getItem('theme') === 'dark'){
+localStorage.removeItem('theme');
+} else {
+  localStorage.setItem('theme','dark')
+}
+addDarkClassToHTML();
+});
+
+function addDarkClassToHTML(){
+   try {
+  if (localStorage.getItem('theme') === 'dark'){
+document.querySelector('html').classList.add('dark');
+document.querySelector('.themetoggle span').textContent = 'nightlight_round';
+} 
+else {
+  document.querySelector('html').classList.remove('dark');
+  document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+}
+   }
+  catch (err) {}
+};
+
+addDarkClassToHTML();
